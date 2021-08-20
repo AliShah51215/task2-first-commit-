29761eff6cf0d7cf2ceedc14714c37e38ca11849
Mateusz Marczak <mateusz.marczak@empik.com>
2021-08-20 19:29:32 +0200

    Initial commit

---
+++ src/main/java/pl/marczak/App.java
@@ -0,0 +1,13 @@
+package pl.marczak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Future Collars!" );
+    }
+}
